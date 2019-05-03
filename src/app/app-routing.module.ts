5f8061d3d66a81005b83bcf581c1c4f404d8b388
Sam Walton <sam@nicheprovider.com>
2019-05-02 21:42:12 -0500

    stage components and partial bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
