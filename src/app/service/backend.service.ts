77d9f4a59e556f0e1f16c0c0a16716e329817a48
Sam Walton <sam@nicheprovider.com>
2020-01-03 20:23:18 -0600

    switch to jsonapi

--- src/app/service/backend.service.ts
+++ src/app/service/backend.service.ts
@@ -4,8 +4,8 @@ import { User } from '../model/user';
 
 
 export const Api = {
-    login : 'https://work.bentwhiskerranch.org/api/v1/auth/sign_in',
-    transactions : 'https://work.bentwhiskerranch.org/jsonapi/node/transaction'
+    login : 'https://works.bentwhiskerranch.org/api/v1/auth/sign_in',
+    transactions : 'https://works.bentwhiskerranch.org/jsonapi/node/transaction'
 };
 
 // export const Api = {
